33125b434a747f96d2f8f6ccfb7e860dd211d846
Sascha Bugor <alexanderbugor@gmail.com>
2020-08-02 20:35:53 +0200

    Add HighlightMatch.test

---
+++ src/components/HighlightMatch/HighlightMatch.test.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Fuse from 'fuse.js';
+import HighlightMatch from './HighlightMatch';
+
+describe('HighlightMatch', () => {
+  describe('no matches in children', () => {
+    it('should render children', () => {
+      const wrapper = shallow(<HighlightMatch>super</HighlightMatch>);
+
+      const strong = wrapper.find('strong');
+
+      expect(strong).not.toExist();
+
+      expect(wrapper.text()).toEqual('super');
+    });
+  });
+
+  describe('matches in children', () => {
+    it('should highlight the match', () => {
+      const matches = [
+        {
+          indices: [[0, 2]],
+          value: 'Pas',
+        },
+      ] as readonly Fuse.FuseResultMatch[];
+
+      const wrapper = shallow(
+        <HighlightMatch matches={matches}>Passionfruit</HighlightMatch>
+      );
+      const strong = wrapper.find('strong');
+
+      expect(strong).toExist();
+      expect(strong.text()).toEqual('Pas');
+
+      const children = wrapper.props().children;
+
+      expect(children).toHaveLength(3);
+      expect(children[0]).toEqual('');
+      expect(children[2]).toEqual('sionfruit');
+    });
+  });
+});
